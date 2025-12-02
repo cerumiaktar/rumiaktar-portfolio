9f9a792ff28d38a62be76e687620dc9d6fb69652
unknown <ce.rumiakter@gmail.com>
2025-12-02 17:43:32 +0600

    update

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -1,17 +1,33 @@
 import { RiSeoLine } from "react-icons/ri";
+import { FaPenNib, FaCode } from "react-icons/fa";
 
 const Skills = () => {
     return (
         <div className='container mx-auto mt-12 mb-12'>
-            <h1 className='text-3xl font-bold text-center'>What I Do</h1>
-            <div>
-                <div className="card bg-base-100 w-96 shadow-2xl">
-                    <figure className="px-10 pt-10">
-                        <span className="text-5xl"><RiSeoLine /></span>
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">SEO</h2>
-                        <p>SEO optimizing your website to improve its visibility on search engines.</p>
+            <div className="flex items-center gap-6">
+                <div className="w-6/12">
+                    <h1 className='text-3xl font-bold'>What I Do</h1>
+                    <p className="leading-8">I specialize in designing user experiences, crafting engaging interfaces, and building robust web applications that deliver value and usability.
+                    My approach combines creativity and technical expertise to deliver solutions that are both visually appealing and highly functional for users.</p>
+                </div>
+                <div className="6/12 space-y-6">
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">SEO</h2>
+                            <p>SEO optimizing your website to improve its visibility on search engines.</p>
+                        </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">UI/UX Design</h2>
+                            <p>UI/UX design is to produce aesthetically pleasing and intuitive digital experiences.</p>
+                        </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Development</h2>
+                            <p>It focuses on creating functional, efficient, and scalable digital solutions, transforming ideas</p>
+                        </div>
                     </div>
                 </div>
             </div>
